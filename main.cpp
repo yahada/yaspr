7db50c27fba154e69bd1083a1031ccbca7d07f99
yaha <niadzy14@gmail.com>
2026-08-01 10:15:43 +0300

    initial + plan

---
+++ main.cpp
@@ -0,0 +1,19 @@
+int main()
+{
+
+  //PART 1
+  //TODO: packet sniffer begin stuff using:
+  //-pcap
+  //-tcpdump
+  //...
+  //GOAL -> look through paket and filter them using easy interface
+
+  //TODO:make explanation  of packet data for some types of packets
+
+  //TODO:make .pcap files reader and writer
+
+  //TODO:make network statistics
+  //
+  //PART2
+  //...
+}
